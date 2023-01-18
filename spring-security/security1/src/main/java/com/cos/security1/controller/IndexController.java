6b24dbabba3f61df62ed088b20005b2f51c9eed9
Choo <choo000407@naver.com>
2023-01-18 21:57:53 +0900

    feat: 회원가입, 암호화

--- spring-security/security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ spring-security/security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,22 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserRepository  userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({"","/"})
     public String index(){
@@ -27,19 +38,26 @@ public class IndexController {
         return "manager";
     }
 
-    // 스프링시큐리티가 해당주소를 낚아채버림. 설정 필요
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
+    // 스프링시큐리티가 해당주소를 낚아채버림. 설정 필요 -> 이제 안낚아챔
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join";
+    // 회원가입 폼
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
     }
 
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨!";
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword=user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user); // 회원가입 잘됨. 비밀번호: 1234 -> 시큐리티로 로그인 할 수 없음. 이유는 패스워드가 암호화가 안되어 있어서.
+        return "redicect:/loginForm";
     }
 }
