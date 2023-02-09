ad020ff1a840393ceec62693d01a6fff771aad50
Choo <choo000407@naver.com>
2023-02-09 21:23:51 +0900

    feat: jwt 임시 토큰 만들어 테스트 (#23)
    security 동작 전에 걸러내야해서 filter3에 넣음

--- spring-security/jwt/src/main/java/com/cos/jwt/config/FilterConfig.java
+++ spring-security/jwt/src/main/java/com/cos/jwt/config/FilterConfig.java
@@ -13,7 +13,7 @@ public class FilterConfig {
     public FilterRegistrationBean<MyFilter1> filter1() {
         FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
         bean.addUrlPatterns("/*");
-        bean.setOrder(0); // 낮은 번호가 필터 중에서 가장 먼저 실행됨
+        bean.setOrder(1); // 낮은 번호가 필터 중에서 가장 먼저 실행됨
         return bean;
     }
 
@@ -21,7 +21,7 @@ public class FilterConfig {
     public FilterRegistrationBean<MyFilter2> filter2() {
         FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
         bean.addUrlPatterns("/*");
-        bean.setOrder(1); // 낮은 번호가 필터 중에서 가장 먼저 실행됨
+        bean.setOrder(0); // 낮은 번호가 필터 중에서 가장 먼저 실행됨
         return bean;
     }
 }
--- spring-security/jwt/src/main/java/com/cos/jwt/filter/MyFilter3.java
+++ spring-security/jwt/src/main/java/com/cos/jwt/filter/MyFilter3.java
@@ -1,7 +1,10 @@
 package com.cos.jwt.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
 
@@ -9,7 +12,32 @@ public class MyFilter3 implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        System.out.println("필터3");
-        chain.doFilter(request, response); // 프로그램 끝나지 말고 프로세스 계속 진행하게 하기 위함
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        // req.setCharacterEncoding("UTF-8"); // Authorization엔 정책 상 한글 들어갈 수 없음
+
+        // 실제 로직
+        // 토큰 이름 : cos <- 이걸 만들어줘야함. id, pw가 정상적으로 들어와서 로그인이 완료되면 토큰을 만들어주고 그걸 응답을 해준다.
+        // 요청할 때마다 header에 Authorization에 value값으로 토큰을 가지고 옴
+        // 그때 토큰이 넘어오면 이 토큰이 내가 만든 토큰이 맞는지만 검증만 하면 됨. (RSA, HS256)
+
+        // 테스트 로직
+        // 토큰 이름 : cos
+        // cos라는 토큰 넘어오면 필터 계속 타서 인증되게하고, 그게 아니면 더이상 필터 못타게 해서 컨트롤러에 진입조차 못하게 함
+        if (req.getMethod().equals("POST")) { // post일 때만 동작
+            System.out.println("POST 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+            System.out.println("필터3");
+
+            if (headerAuth.equals("cos")) {
+                chain.doFilter(req, res); // 프로그램 끝나지 말고 프로세스 계속 진행하게 하기 위함
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증 안됨");
+            }
+        }
     }
 }
