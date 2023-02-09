5c080c4d459fe160dcb14c3c0208b4a94ca8c3e0
Choo <choo000407@naver.com>
2023-02-10 04:10:45 +0900

    feat: JwtProperties 적용 (#27)

--- spring-security/jwt/src/main/java/com/cos/jwt/config/jwt/JwtAuthorizationFilter.java
+++ spring-security/jwt/src/main/java/com/cos/jwt/config/jwt/JwtAuthorizationFilter.java
@@ -37,19 +37,19 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
         System.out.println("인증이나 권한이 필요한 주소 요청이 됨");
 
-        String jwtHeader = request.getHeader("Authorization");
+        String jwtHeader = request.getHeader(JwtProperties.HEADER_STRING);
         System.out.println("jwtHeader : " + jwtHeader);
 
         // header가 있는지 확인
-        if (jwtHeader == null || !jwtHeader.startsWith("Bearer ")) {
+        if (jwtHeader == null || !jwtHeader.startsWith(JwtProperties.TOKEN_PREFIX)) {
             chain.doFilter(request, response);
             return;
         }
 
         // JWT 토큰을 검증을 해서 정상적인 사용자인지 확인
-        String jwtToken = request.getHeader("Authorization").replace("Bearer ","");
+        String jwtToken = request.getHeader(JwtProperties.HEADER_STRING).replace(JwtProperties.TOKEN_PREFIX,"");
 
-        String username = JWT.require(Algorithm.HMAC512("cos")).build().verify(jwtToken).getClaim("username").asString();
+        String username = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(jwtToken).getClaim("username").asString();
 
         // 서명이 정상적으로 됨
         if(username != null) {
