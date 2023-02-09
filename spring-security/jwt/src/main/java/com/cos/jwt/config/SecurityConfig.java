5480dcecd5c1b6d04cc0e3b0968ecf25c277139b
Choo <choo000407@naver.com>
2023-02-09 20:52:26 +0900

    feat: [22] jwt Filter 등록
    Security filter이 다 끝나고 나서 MyFilter가 실행됨
    MyFilter가 Spring security보다 먼저 동작하게 하고 싶다면 before로 걸어야함
    3->security filter->1->2

--- spring-security/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ spring-security/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -18,6 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class); // using addfilterBefore or addFilterAfter -> 이거 대신 FilterConfig 만들어줌
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안한단 의미
---
+++ spring-security/jwt/src/main/java/com/cos/jwt/filter/MyFilter3.java
@@ -0,0 +1,15 @@
+package com.cos.jwt.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyFilter3 implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println("필터3");
+        chain.doFilter(request, response); // 프로그램 끝나지 말고 프로세스 계속 진행하게 하기 위함
+    }
+}
